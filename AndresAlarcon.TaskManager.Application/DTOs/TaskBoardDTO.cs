4d09733fe4e27ce76583bb8c3d0458a83d58744e
Andres Alarcon <aalarcon@transfiriendo.com>
2024-09-28 18:47:18 -0500

    fix

--- AndresAlarcon.TaskManager.Application/DTOs/TaskBoardDTO.cs
+++ AndresAlarcon.TaskManager.Application/DTOs/TaskBoardDTO.cs
@@ -8,9 +8,11 @@
         public DateTime DueDate { get; set; }
         public string Status { get; set; }
         public string Priority { get; set; }
-        public Guid? AssignedTo { get; set; }
+        public Guid AssignedTo { get; set; }
         public bool IsActive { get; set; }
         public DateTime CreatedOn { get; set; }
         public Guid CreatedBy { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public Guid UpdatedBy { get; set; }
     }
 }
